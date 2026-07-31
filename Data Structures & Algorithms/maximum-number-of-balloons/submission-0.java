e98f0d05fdd783b23e337848f0d6d62f3b49d497
fuzaylameen <85730076+fuzaylameen@users.noreply.github.com>
2026-07-31 23:47:43 +0530

    Add: maximum-number-of-balloons - submission-0

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int n=text.length();
+        for(int i=0;i<n;i++ ){
+            char c=text.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        int c=1000;
+        String check="balon";
+        for(int i=0;i<check.length();i++){
+            char t=check.charAt(i);
+            int m=map.getOrDefault(t,0);
+            if(t=='l' || t=='o') m=m/2;
+            if (m<c) c=m;
+        }
+        return c;
+
+        
+    }
+}
